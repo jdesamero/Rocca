1c2a8d035c1055f08e36a672bd31d86f73cb6a1c
Joel Desamero <joel@geekoracle.com>
2016-06-10 09:33:55 -0400

    Fix bug: path was changed so unit test was failing

--- serverside/library/RoccaTest/Debug.php
+++ serverside/library/RoccaTest/Debug.php
@@ -12,7 +12,7 @@ class RoccaTest_Debug extends Rocca_UnitTest
 		// below is line 13
 		$sOut = Rocca_Debug::getLine( 'Unit testing...', __FILE__, __LINE__, __METHOD__ );
 		
-		$bRes = strpos( $sOut, "Rocca/library/RoccaTest/Debug.php: Line 13: RoccaTest_Debug::run(): Unit testing...\n" );
+		$bRes = strpos( $sOut, "Rocca/serverside/library/RoccaTest/Debug.php: Line 13: RoccaTest_Debug::run(): Unit testing...\n" );
 		
 		$this->assertStrictlyEqual( 'get line', TRUE, ( $bRes !== FALSE ) );
 		
@@ -20,7 +20,7 @@ class RoccaTest_Debug extends Rocca_UnitTest
 		// below is line 21		
 		$sOut2 = Rocca_String::fromOb( [ 'Rocca_Debug', 'showLine' ], 'More unit testing...', __FILE__, __LINE__, __METHOD__ );
 		
-		$bRes2 = strpos( $sOut2, "Rocca/library/RoccaTest/Debug.php: Line 21: RoccaTest_Debug::run(): More unit testing...\n" );
+		$bRes2 = strpos( $sOut2, "Rocca/serverside/library/RoccaTest/Debug.php: Line 21: RoccaTest_Debug::run(): More unit testing...\n" );
 		
 		$this->assertStrictlyEqual( 'show line', TRUE, ( $bRes !== FALSE ) );
 		
